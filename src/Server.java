565734d1033feb9279686634a8e47372e4f40c85
Juan P. Rostran <54974335+juanprrr@users.noreply.github.com>
2020-02-29 23:41:26 -0600

    [Test] Server GUI body

--- src/Server.java
+++ src/Server.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
+import javax.swing.*;
+import java.io.*;
+import java.util.*;
+import java.net.*;
+
 
 public class Server {
+    private JButton msg_send;
+    private JTextField msg_text;
+    private JTextArea msg_area;
+    private JTextPane connectedClients;
 }
