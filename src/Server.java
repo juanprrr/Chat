b3938aba2d2b9d33de99920659c68d3c52ff416b
Juan P. Rostran <54974335+juanprrr@users.noreply.github.com>
2020-03-01 01:17:06 -0600

    [Add] Server proof #3

--- src/Server.java
+++ src/Server.java
@@ -9,4 +9,19 @@ public class Server {
     private JTextField msg_text;
     private JTextArea msg_area;
     private JTextPane connectedClients;
+
+    public static void main(String[] args) {
+        int port = 9000;
+        try {
+            ServerSocket serverSocket= new ServerSocket(port);
+            while(true){
+                Socket clientSocket = serverSocket.accept();
+                OutputStream outputStream = clientSocket.getOutputStream();
+                outputStream.write("Hola mundo\n".getBytes());
+                clientSocket.close();
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 }
